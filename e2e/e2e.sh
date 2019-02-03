afd0ff1a9057755cb4f55d4c9c3f0ddfdee34ad2
Matti Paksula <matti.paksula@iki.fi>
2019-02-03 16:51:27 +0200

    remove e2e

--- e2e/e2e.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-cd e2e/simple
-  ../../exe/gmortar build asdf
-cd ../..
-
-exe/gmortar build asdf e2e/simple
-exe/gmortar build asdf e2e/custom_dockerfile --dockerfile Dockerfile.custom
-exe/gmortar build asdf e2e/nested_dockerfile --dockerfile layer/Dockerfile
